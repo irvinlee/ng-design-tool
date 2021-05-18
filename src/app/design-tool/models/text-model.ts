aa9b99185b5b13635d6913f2c95e1c4eeca45ae0
Irvin Lee Eduliantes <irvin@gorillaspace.co>
2021-05-18 22:37:35 +0800

    text element hover status

--- src/app/design-tool/models/text-model.ts
+++ src/app/design-tool/models/text-model.ts
@@ -6,7 +6,7 @@ import { TextAlign } from '../types/text-align.enum';
 
 export class TextModel implements Text{
   value = 'New Text Element';
-  coordinates = {top: 10, left: 10} as Coordinates;
+  coordinates = {top: 100, left: 100} as Coordinates;
   format = {
     font: 'Arial',
     size: 12,
@@ -26,30 +26,29 @@ export class TextModel implements Text{
   }
 
   checkIsHovered(mouseX: number, mouseY: number): boolean {
-    return mouseX >= this.metrics.actualBoundingBoxLeft &&
-          mouseX <= this.metrics.actualBoundingBoxRight &&
-          mouseY >= this.metrics.actualBoundingBoxDescent &&
-          mouseY <= this.metrics.actualBoundingBoxAscent;
+    return mouseX >= this.coordinates.left - 5 &&
+          mouseX <= this.coordinates.left + this.metrics.width + 10 &&
+          mouseY >= this.coordinates.top - this.metrics.actualBoundingBoxAscent - 5 &&
+          mouseY <= this.coordinates.top;
   }
 
   renderToCanvas(canvasContext: CanvasRenderingContext2D): void{
+    canvasContext.textBaseline = 'bottom';
     canvasContext.fillStyle = this.format.color;
     canvasContext.font = `${this.format.size}px ${this.format.font}`;
     this.metrics = canvasContext.measureText(this.value);
-    // if (this.isHovered) {
-    //   canvasContext.fillStyle = 'red';
-    // }
+
     canvasContext.fillText(this.value, this.coordinates.left, this.coordinates.top);
 
-    // if (this.isHovered) {
-    //   canvasContext.beginPath();
-    //   canvasContext.rect(
-    //     this.metrics.actualBoundingBoxLeft,
-    //     this.metrics.actualBoundingBoxDescent,
-    //     this.metrics.width,
-    //     this.metrics.actualBoundingBoxDescent
-    //   );
-    //   canvasContext.stroke();
-    // }
+    if (this.isHovered) {
+      canvasContext.beginPath();
+      canvasContext.rect(
+        this.coordinates.left - 5,
+        this.coordinates.top - this.metrics.actualBoundingBoxAscent - 5,
+        this.metrics.width + 10,
+        this.metrics.actualBoundingBoxAscent + 5
+      );
+      canvasContext.stroke();
+    }
   }
 }
