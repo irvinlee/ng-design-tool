9ab837dc4e0adee1383c1ff503473e60352565ba
Irvin Lee Eduliantes <irvin@gorillaspace.co>
2021-06-01 23:55:31 +0800

    unsubscribed onDestroy

--- src/app/design-tool/modules/toolbar/view-options-panel/view-options-panel.component.ts
+++ src/app/design-tool/modules/toolbar/view-options-panel/view-options-panel.component.ts
@@ -1,19 +1,25 @@
+import { Subscription } from 'rxjs';
 import { DesignToolService } from './../../../design-tool.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-view-options-panel',
   templateUrl: './view-options-panel.component.html',
   styleUrls: ['./view-options-panel.component.scss']
 })
-export class ViewOptionsPanelComponent implements OnInit {
+export class ViewOptionsPanelComponent implements OnInit, OnDestroy {
   zoomLevel = 1;
+  private subscriptions?: Array<Subscription>;
 
   constructor(private designToolService: DesignToolService) {
-    this.designToolService.zoomLevel.subscribe(newZoomLevel => this.zoomLevel = newZoomLevel);
+    this.subscriptions?.push(this.designToolService.zoomLevel.subscribe(newZoomLevel => this.zoomLevel = newZoomLevel));
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions?.forEach(subscription => subscription.unsubscribe());
   }
 
   onChangeZoomLevel(e: Event): void {
