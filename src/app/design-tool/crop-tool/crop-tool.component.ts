f2a18841c73c061ed5bdeca6bd80c7bdf964d544
Irvin Lee Eduliantes <irvin@gorillaspace.co>
2021-06-02 11:55:43 +0800

    add resize logic to crop tool

--- src/app/design-tool/crop-tool/crop-tool.component.ts
+++ src/app/design-tool/crop-tool/crop-tool.component.ts
@@ -1,3 +1,4 @@
+import { ElementMouseHandles } from './../types/element-mouse-handles.enum';
 import { CropParams } from './../types/crop-params';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
@@ -22,4 +23,53 @@ export class CropToolComponent implements OnInit {
     this.cropParams = {left: this.left, top: this.top, width: this.width, height: this.height};
   }
 
+  private resizeNW(mouseX: number, mouseY: number): void {
+    const oldYCoord = this.cropParams.top as number;
+    const oldXCoord = this.cropParams.left as number;
+    const width = (this.cropParams.width as number);
+    const height = (this.cropParams.height as number);
+
+    this.cropParams.left = mouseX;
+    this.cropParams.top = mouseY;
+
+    this.cropParams.width = width + (oldXCoord - this.left);
+    this.cropParams.height = height + (oldYCoord - this.top);
+  }
+
+  private resizeNE(mouseX: number, mouseY: number): void {
+    const oldYCoord = this.cropParams.top as number;
+    const width = (this.cropParams.width as number);
+    const height = (this.cropParams.height as number);
+
+    this.cropParams.top = mouseY;
+    this.cropParams.width = width - ((this.cropParams.left as number) + width - mouseX);
+    this.cropParams.height = height + (oldYCoord - this.cropParams.top);
+  }
+
+  private resizeSW(mouseX: number, mouseY: number): void {
+    const oldXCoord = this.cropParams.left as number;
+    const width = (this.cropParams.width as number);
+    const height = (this.cropParams.height as number);
+
+    this.cropParams.left = mouseX;
+    this.cropParams.width = width + (oldXCoord - (this.cropParams.left as number));
+    this.cropParams.height = height - ((this.cropParams.top as number) + height - mouseY);
+  }
+
+  private resizeSE(mouseX: number, mouseY: number): void {
+    const width = (this.cropParams.width as number);
+    const height = (this.cropParams.height as number);
+
+    this.cropParams.width = width - ((this.cropParams.left as number) + width - mouseX);
+    this.cropParams.height = height - ((this.top as number) + height - mouseY);
+  }
+
+  resize(mouseHandleUsed: string, mouseX: number, mouseY: number): void {
+    switch (mouseHandleUsed) {
+      case ElementMouseHandles.TOP_LEFT: this.resizeNW(mouseX, mouseY); break;
+      case ElementMouseHandles.TOP_RIGHT: this.resizeNE(mouseX, mouseY); break;
+      case ElementMouseHandles.BOTTOM_LEFT: this.resizeSW(mouseX, mouseY); break;
+      case ElementMouseHandles.BOTTOM_RIGHT: this.resizeSE(mouseX, mouseY); break;
+    }
+  }
 }
